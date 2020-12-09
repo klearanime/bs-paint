5d20c05116c9e822e75530948e829e0652dd28b4
Crystal K Young <maybiz@hotmail.com>
2020-12-09 11:51:10 -0500

    done

--- main.js
+++ main.js
@@ -25,6 +25,8 @@ while (count <= gridWidth * gridWidth) {
   count++;
 }
 
+
+
 // You probably should NOT do these in the order below.
 // That is, you probably should NOT do all the queries,
 // THEN all the functions,
@@ -49,25 +51,75 @@ while (count <= gridWidth * gridWidth) {
 // Add queries for all your squares, palette colors, and brush here.
 // (Note the singular or plural used in that sentence!)
 
+const squares = document.querySelectorAll('.square')
+const paintBrush = document.querySelector('.brush')
+const colorPalette = document.querySelectorAll('.palette-color')
+const color1 = document.querySelector('palette-color.color-1')
+const color2 = document.querySelector('palette-color.color-2')
+const color3 = document.querySelector('palette-color.color-3')
+const color4 = document.querySelector('palette-color.color-4')
+const color5 = document.querySelector('palette-color.color-5')
+const currentColor = document.querySelector('.current-brush')
+const canvas = document.querySelector('.canvas')
 
 
 /****************************
  * EVENT LISTENER FUNCTIONS *
 ****************************/
 
-// Now add some functions to handle clicking one particular square
-// and clicking one particular palette color. You can leave them
-// empty at first, though a console.log just to know they're being
-// run as event listeners (after the next step is set up) isn't a
-// bad idea for testing purposes.
 
+// const square = squares.forEach.addEventListener('click', canvas)
 
+// const paletteColors = colorPalette.forEach.addEventListener('click', colorPalette)
+
+// const pickColor = squares.addEventListener('click', square)
 
 /**************************
  * WIRING IT ALL TOGETHER *
 **************************/
 
+
 // Now: wiring up our event listeners to our html node elements.
 // You'll need to add the appropriate event listener for each
 // square and for each palette color from the functions you
 // wrote above.
+
+
+const pickColor = (element) => {
+  return element.classList[1]
+}
+
+
+const changeClickedSquare = (event) => {
+  const whatWasPicked = event.target
+  whatWasPicked.classList.replace(pickColor(whatWasPicked), pickColor(currentColor))
+}
+
+
+for (const square of squares) {
+  square.addEventListener('click', changeClickedSquare)
+}
+
+
+const changePaletteColor = (event) => {
+  whatWasPicked = event.target
+  currentColor.classList.replace(pickColor(currentColor), pickColor(whatWasPicked))
+}
+
+
+for (const color of colorPalette) {
+  color.addEventListener('click', changePaletteColor)
+}
+
+// ------------------------------------------
+const todoItems = document.querySelectorAll('.todo')
+
+const print = (event) => {
+  const whatWasClicked = event.target
+  whatWasClicked.style.color = 'red'
+  whatWasClicked.innerText = 'done'
+}
+
+for (const todo of todoItems) {
+  todo.addEventListener('click', print)
+}
